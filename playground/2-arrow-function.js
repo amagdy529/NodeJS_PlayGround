905a54597cba4fc9b04d920622e4b5117bb07bc5
amagdy529 <amagdy529@gmail.com>
2019-04-06 18:11:04 +0200

    arrow functions

---
+++ playground/2-arrow-function.js
@@ -0,0 +1,25 @@
+// const square = function (x) {
+//     return x * x
+// }
+
+// const square = (x) => {
+//     return x * x
+// }
+
+// const square = (x) => x * x
+
+// console.log(square(4))
+
+
+const event = {
+    name: 'Party',
+    guestList: ['ahmed','mike','robert'],
+    printGuestList() {
+        console.log('Guest List for ' + this.name)
+        this.guestList.forEach((guest) => {
+            console.log(guest + ' is attending ' + this.name)
+        })
+    }
+}
+
+event.printGuestList()
