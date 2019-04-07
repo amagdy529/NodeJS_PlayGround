edf0191fc9655cd59789ca35b6d5565e61abdbee
amagdy529 <amagdy529@gmail.com>
2019-04-07 20:02:38 +0200

    Weather app:appjs

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,10 @@
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+
+const url = 'https://api.darksky.net/forecast/87fac29a8c1a9657793a3c171506b26c/37.8267,-122.4233'
+
+request({ url:url , json: true } ,( error,response )=>{
+    // console.log(response.body.currently)
+    var temp = response.body.currently.temperature
+    var rain = response.body.currently.precipProbability
+    console.log('it is currently ' + temp + ' degrees out. there is a ' + rain + '% chance of rain')
+})
