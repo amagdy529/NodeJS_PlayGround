f1791fce392b95c2a60ac0e4bed72f1770624bcf
amagdy529 <amagdy529@gmail.com>
2019-04-07 22:27:09 +0200

    weather-app: utils module , geo fucking code

--- weather-app/app.js
+++ weather-app/app.js
@@ -15,42 +15,6 @@ var request = require('request');
 // })
 
 
-// GEOCoding
-//Address -> Lat/Long ->
-
-// const Geourl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW1hZ2R5NTI5IiwiYSI6ImNqdTc5MWs5YTFydm00ZXBkOHQ0ZXdxNHQifQ.-a2d1S7CjJaPLUVpZc4vNQ'
-
-// request({ url:Geourl , json: true } ,( error,response )=>{
-//     if (error) {
-//         console.log('Unable to connect to the MapBox Geo  service!')
-//     } else if (response.body.features.length === 0) {
-//         console.log('Unable to find Location , try another search!')
-//     } else {
-//         const latitude = response.body.features[0].center[1]
-//         const longitude = response.body.features[0].center[0]
-//         console.log(latitude,longitude)
-//     }
-// })
-
-
-const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYW1hZ2R5NTI5IiwiYSI6ImNqdTc5MWs5YTFydm00ZXBkOHQ0ZXdxNHQifQ.-a2d1S7CjJaPLUVpZc4vNQ'
-
-    request({url: url,json:true} , (error, response) => {
-        if (error) {
-            callback('Unable To connect To Location Services!',undefined)
-        } else if (response.body.features.length === 0) {
-            callback('Unable To find location , try another location' , undefined)   
-        } else {
-            callback(undefined , {
-                latitude: response.body.features[0].center[1] ,
-                longitude: response.body.features[0].center[0] ,
-                location: response.body.features[0].place_name
-            })
-        }
-    })
-
-}
 
 geocode('Cairo', (error,data) => {
     console.log('error is : ' , error)
